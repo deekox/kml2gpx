d94ae2d4161a3e179e90fdeddb79588a26f9d76c
deekox <deekox@gmail.com>
2024-05-21 19:39:51 +0200

    input/output file name handling

--- kml2gpx.py
+++ kml2gpx.py
@@ -1,5 +1,26 @@
+from genericpath import isfile
+import os
+import sys
+
+usageString=f"usage: {sys.argv[0]} <kml/kmz file> [output_file_name]"
+
 if __name__ != "__main__":
-    print("usage: %{sys.arg[0]} kml/kmz file")
+    print(usageString)
     exit(1)
 
-print("Hello")
+print(f"len(sys.argv)={len(sys.argv)}")
+
+if len(sys.argv) != 2 and len(sys.argv) != 3:
+    print(usageString)
+    exit(1)
+
+inputFilePath=sys.argv[1]
+outputFilePath=""
+if len(sys.argv) == 2:
+    outputFilePath = os.path.basename(os.path.splitext(os.path.basename(inputFilePath))[0]) + ".gpx"
+elif sys.argv[2].endswith(".gpx"):
+    outputFilePath = sys.argv[2]
+else:
+    outputFilePath = sys.argv[2] + ".gpx"
+
+print(f"input {inputFilePath}\noutput: {outputFilePath}")
