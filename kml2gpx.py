6c15059561ead9fa8ee0629471e97e981ec2832c
deekox <deekox@gmail.com>
2024-05-22 18:41:55 +0200

    added support for multiple Placemark elements

--- kml2gpx.py
+++ kml2gpx.py
@@ -5,6 +5,21 @@ import xml.etree.ElementTree as ET
 
 usageString=f"usage: {sys.argv[0]} <kml/kmz file> [output_file_name]"
 
+def createGPX(coordinates, outputPath):
+    newRoot = ET.Element('gpx')
+    gtrk = ET.SubElement(newRoot, 'trk')
+    trkSeg = ET.SubElement(gtrk, 'trkseg')
+    for line in coordinates.text.splitlines():
+        if len(line) > 0:
+            vals = line.strip().split(',')
+            if len(vals) >= 2:
+                trkpt = ET.SubElement(trkSeg, "trkpt")
+                trkpt.set("lon", vals[0])
+                trkpt.set("lat", vals[1])
+    tree = ET.ElementTree(newRoot)
+    tree.write(outputPath)
+
+
 if __name__ != "__main__":
     print(usageString)
     exit(1)
@@ -16,11 +31,12 @@ if len(sys.argv) != 2 and len(sys.argv) != 3:
 inputFilePath=sys.argv[1]
 outputFilePath=""
 if len(sys.argv) == 2:
-    outputFilePath = os.path.basename(os.path.splitext(os.path.basename(inputFilePath))[0]) + ".gpx"
+    outputFilePath = os.path.basename(os.path.splitext(os.path.basename(inputFilePath))[0])
 elif sys.argv[2].endswith(".gpx"):
-    outputFilePath = sys.argv[2]
+    outputFilePath = sys.argv[2][:-4]
 else:
-    outputFilePath = sys.argv[2] + ".gpx"
+    outputFilePath = sys.argv[2]
+
 
 inputFileExtension = os.path.basename(os.path.splitext(os.path.basename(inputFilePath))[1])
 
@@ -35,23 +51,20 @@ if root == None:
     sys.exit(1)
 
 namespaces = { "ns" : root.tag[1:-4] }
-coordinates = root.find('ns:Document/ns:Placemark/ns:LineString/ns:coordinates', namespaces)
-if coordinates != None:
-    newRoot = ET.Element('gpx')
-    gtrk = ET.SubElement(newRoot, 'trk')
-    trkSeg = ET.SubElement(gtrk, 'trkseg')
-    for line in coordinates.text.splitlines():
-        if len(line) > 0:
-            vals = line.strip().split(',')
-            if len(vals) >= 2:
-            # print(f">>{line}<<, len: {len(line)}  len(vals)= {len(vals)}")
-            #   print(f"{vals[0]}   {vals[1]}") 
-              trkpt = ET.SubElement(trkSeg, "trkpt")
-              trkpt.set("lon", vals[0])
-              trkpt.set("lat", vals[1])
-    tree = ET.ElementTree(newRoot)
-    tree.write(outputFilePath)
-else:
+cnt = 0
+for coordinates in root.findall('ns:Document/ns:Placemark/ns:LineString/ns:coordinates', namespaces):
+    cnt = cnt + 1
+if cnt == 0:
     print("Unable to find <coordinates> element in .klm file. Giving up :(")
     sys.exit(1)
+else:
+    i = 1
+    for placemark in root.findall('ns:Document/ns:Placemark', namespaces):
+        name = placemark.find('ns:name', namespaces)
+        filename = outputFilePath + f"_{i}-{cnt}_" + name.text.strip() + ".gpx"
+        coordinates = placemark.find('ns:LineString/ns:coordinates', namespaces)
+        if coordinates != None:
+            createGPX(coordinates, filename)
+            i = i + 1
+
 
